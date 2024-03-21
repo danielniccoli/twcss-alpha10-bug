dec9c79857e82d4833d810cf08e1a046c0642734
Daniel Niccoli <2971735+danielniccoli@users.noreply.github.com>
2024-03-21 23:16:58 +0100

    Add files

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+    import Navigation from "./Navigation.svelte"
+    import "./main.css"
+</script>
+
+<div class="min-h-full">
+    <Navigation />
+
+    <header class="bg-white shadow">
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <h1 class="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
+        </div>
+    </header>
+
+    <main>
+        <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+            <slot />
+        </div>
+    </main>
+</div>
